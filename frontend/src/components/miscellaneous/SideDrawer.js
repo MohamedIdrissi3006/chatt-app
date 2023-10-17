e2d2990f86e5de401bc0f465d2bc16ca80cc27de
MohamedIdrissi3006 <87528955+MohamedIdrissi3006@users.noreply.github.com>
2023-10-17 01:33:26 +0100

    Update SideDrawer.js

--- frontend/src/components/miscellaneous/SideDrawer.js
+++ frontend/src/components/miscellaneous/SideDrawer.js
@@ -75,7 +75,7 @@ function SideDrawer() {
         },
       };
 
-      const { data } = await axios.get(`/api/user?search=${search}`, config);
+      const { data } = await axios.get(`https://chatt-app-theta.vercel.app/api/user?search=${search}`, config);
 
       setLoading(false);
       setSearchResult(data);
@@ -102,7 +102,7 @@ function SideDrawer() {
           Authorization: `Bearer ${user.token}`,
         },
       };
-      const { data } = await axios.post(`/api/chat`, { userId }, config);
+      const { data } = await axios.post(`https://chatt-app-theta.vercel.app/api/chat`, { userId }, config);
 
       if (!chats.find((c) => c._id === data._id)) setChats([data, ...chats]);
       setSelectedChat(data);
